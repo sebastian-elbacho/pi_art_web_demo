04ee6d191fd898ed64b8ba22f219249e855b9bc4
SebastianK <sebastian.bachowski@gmail.com>
2025-09-28 21:12:03 +0100

    routes added

--- app.py
+++ app.py
@@ -6,5 +6,32 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route('/fireshow')
+def fireshow():
+    return render_template('fireshow.html')
+
+@app.route('/lightshow')
+def lightshow():
+    return render_template('lightshow.html')
+
+@app.route('/lasershow')
+def lasershow():
+    return render_template('lasershow.html')
+
+@app.route('/ilusionist')
+def ilusionist():
+    return render_template('ilusionist.html')
+
+@app.route('/aerial')
+def aerial():
+    return render_template('aerial.html')
+
+
+
+
+
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
